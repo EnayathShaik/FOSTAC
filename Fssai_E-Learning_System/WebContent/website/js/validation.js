54a9d4ce63f5155023005ee33a27caceba486969
zentech-fostac <shaik.etu@gmail.com>
2016-12-11 15:46:01 +0530

    increase validation

--- Fssai_E-Learning_System/WebContent/website/js/validation.js
+++ Fssai_E-Learning_System/WebContent/website/js/validation.js
@@ -1,7 +1,7 @@
 function allLetter(id, inputtxt) {
-	//var letters = /^[A-Za-z]+$/; //With out Space
-	var letters = /^[a-zA-Z\s]+$/; //With Space only
-	if(inputtxt.length < 0){
+	// var letters = /^[A-Za-z]+$/; //With out Space
+	var letters = /^[a-zA-Z\s]+$/; // With Space only
+	if (inputtxt.length < 0) {
 		if (inputtxt.match(letters)) {
 			return true;
 		} else {
@@ -12,22 +12,25 @@ function allLetter(id, inputtxt) {
 	}
 }
 
-
 function allnumeric(id, inputtxt) {
 	var numbers = /^[0-9]+$/;
-	if (inputtxt.match(numbers)) {
-		return true;
-	} else {
-		document.getElementById(id).value = "";
-		alert('Please input numeric characters only');
-		return false;
+	if (inputtxt.length < 0) {
+		if (inputtxt.match(numbers)) {
+			return true;
+		} else {
+			document.getElementById(id).value = "";
+			alert('Please input numeric characters only');
+			return false;
+		}
 	}
 }
 
-function pan_validate(id, pan) {
+function pan_validate(id, inputtxt) {
+	if (inputtxt.length < 0) {
 	var regpan = /^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/;
 	if (regpan.test(pan) == false) {
 		document.getElementById(id).value = "";
 		alert('INVALID PAN')
+	}
 	}
 }
